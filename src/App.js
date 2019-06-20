7e1c5ab6e87d464d5b77c640ac5530c024c30771
billorer <zoran@suto.ro>
2019-06-20 23:35:36 +0200

    started creating components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import React, { Component } from 'react';
+import React, { Fragment, Component } from 'react';
+import UserItem from './components/users/UserItem';
+import Navbar from './components/layout/NavBar';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className='App'>
-        <h1>Hello from react</h1>
-        <label htmlFor='name'>Name</label>
+        <Navbar />
+        <UserItem />
       </div>
     );
   }
