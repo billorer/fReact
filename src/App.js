e6c02d33f053ba4eba4681601e284efc6374a8b8
zoran.suto <zoran.suto@blackbelt.hu>
2019-06-21 09:39:32 +0200

    react spinner

--- src/App.js
+++ src/App.js
@@ -1,14 +1,29 @@
-import React, { Fragment, Component } from 'react';
-import UserItem from './components/users/UserItem';
+import React, { Component } from 'react';
+import Users from './components/users/Users';
 import Navbar from './components/layout/NavBar';
+import axios from 'axios';
 import './App.css';
 
 class App extends Component {
+  state = {
+    users: [],
+    loading: false
+  };
+
+  async componentDidMount() {
+    this.setState({ loading: true });
+
+    const res = await axios.get('https://api.github.com/users');
+    this.setState({ users: res.data, loading: false });
+  }
+
   render() {
     return (
       <div className='App'>
         <Navbar />
-        <UserItem />
+        <div className='container'>
+          <Users loading={this.state.loading} users={this.state.users} />
+        </div>
       </div>
     );
   }
